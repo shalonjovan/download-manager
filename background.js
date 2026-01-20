a3b5803e0e0f8d70a3d33e24636e123e9bd6c8a6
shalonjovan <shalonjovan@gmail.com>
2026-01-21 01:52:26 +0530

    added the redownload logic as a set

--- background.js
+++ background.js
@@ -1,15 +1,21 @@
-let isRe=false;
+// let isRe=false;
+
+const reDownloads=new Set();
 
 function getPath(oldpath){
   let parts =oldpath.split("/");
   let filename =parts.pop();
-  let newpath="ReDownloads/"+parts.join("/")+"/"+filename;
+  let newpath="ReDownloads/"+filename;
   return newpath;
 }
 
 browser.downloads.onCreated.addListener(async (download) => {
-    if(isRe){
-        return;
+    // if(isRe){
+    //     return;
+    // }
+    if(reDownloads.has(download.id)){
+      reDownloads.delete(download.id);
+      return;
     }
 
     console.log("download stopped:",download);
@@ -18,19 +24,25 @@ browser.downloads.onCreated.addListener(async (download) => {
       await browser.downloads.cancel(download.id);
       await browser.downloads.erase({id: download.id});
 
-      isRe=true;
+      // isRe=true;
 
-      await browser.downloads.download({
+      let newid =await browser.downloads.download({
         url: download.url,
         filename: getPath(download.filename),
         conflictAction: "uniquify",
         saveAs: false,
       });
 
+      reDownloads.add(newid);
+
       console.log("re download started baby");
     }catch(e){
       console.error("error in re download",e);
-    }finally{
-      isRe=false;
     }
+});
+
+browser.downloads.onChanged.addListener(delta => {
+  if(delta.state && (delta.state.current === "complete" || delta.state.current === "interrupted")){
+    reDownloads.delete(delta.id);
+  }
 });
