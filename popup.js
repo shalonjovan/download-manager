8ea3e29c23eed3c461d923633b20658c4b296ade
shalonjovan <shalonjovan@gmail.com>
2026-01-23 15:39:47 +0530

    some changes

---
+++ popup.js
@@ -0,0 +1,15 @@
+const searchInput=document.getElementById("search");
+const results=document.getElementById("result");
+
+const darkModeToggle=document.getElementById("darkMode");
+
+const nameInput=document.getElementById("name");
+const domainInput=document.getElementById("domain");
+const extInput=document.getElementById("ext");
+const pathInput=document.getElementById("path");
+
+const saveBtn=document.getElementById("save");
+const deleteBtn=document.getElementById("delete");
+const status=document.getElementById("status");
+
+
